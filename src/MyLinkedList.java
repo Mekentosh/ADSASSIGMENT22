9b481c6f7326b34df29832830c76d3f9b8702c7a
Mekentosh <m87022928044@gmail.com>
2023-07-24 03:33:05 +0600

    8

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -12,5 +12,75 @@ public class MyLinkedList<E> implements MyList<E> {
     private Node head;
     private Node tail;
     private int size;
+    @Override
+    public void add(E element){
+        Node newNode = new Node(element, null, tail);
+        if (tail == null){
+            head = newNode;
+        }
+        else{
+            tail.next = newNode;
+        }
+        tail = newNode;
+        size++;
+    }
+    public void checkIndex(int index){
+        if (index < 0 || index >= this.size){
+            throw new IndexOutOfBoundsException();
+        }
+    }
+    @Override
+    public E get(int index){
+        checkIndex(index);
 
+        Node current;
+        if (index <= size / 2) {
+            current = head;
+            for (int i = 0; i < index; i++) {
+                current = current.next;
+            }
+        }
+        else {
+            current = tail;
+            for (int i = size - 1; i > index; i--) {
+                current = current.prev;
+            }
+        }
+        return current.element;
+    }
+    @Override
+    public E remove(int index){
+        checkIndex(index);
+        Node current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        if (current.prev == null) {
+            head = current.next;
+        } else {
+            current.prev.next = current.next;
+        }
+        if (current.next == null) {
+            tail = current.prev;
+        } else {
+            current.next.prev = current.prev;
+        }
+        size--;
+        return current.element;
+    }
+    @Override
+    public int size() {
+        return size;
+    }
+    @Override
+    public boolean contains(Object o) {
+        Node current = head;
+        while (current != null) {
+            if (current.element.equals(o)) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
 }
