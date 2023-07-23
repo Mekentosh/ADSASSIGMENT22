56fec58f31a832b8c98fe2d9177a944f4b9ca98c
Mekentosh <m87022928044@gmail.com>
2023-07-24 03:03:31 +0600

    3

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,2 +1,10 @@
-public class MyArrayList {
+public class MyArrayList<T> implements MyList<T> {
+    //Define a private instance variable of type Object[] to hold the elements of the list.
+    private Object[] array;
+    //Define an int variable called size to keep track of the number of elements in the list.
+    private int size = 0;
+
+
+
+
 }
