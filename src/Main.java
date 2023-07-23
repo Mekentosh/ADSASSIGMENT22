6a4ffaed8a6aed5b9b1c1888df9acb354dc9da14
Mekentosh <m87022928044@gmail.com>
2023-07-24 03:12:55 +0600

    4

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
 public class Main {public static void main(String[] args) {
+    MyArrayList<Integer> array = new MyArrayList<>();
+    array.add(1);
+    array.add(1);
+    array.add(1);
+    array.add(2);
+    array.add(2);
+    System.out.println("Array before deleting duplicates: ");
 
 }
 
--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class MyArrayList<T> implements MyList<T> {
     //Define a private instance variable of type Object[] to hold the elements of the list.
     private Object[] array;
@@ -36,14 +38,87 @@ public class MyArrayList<T> implements MyList<T> {
 
     }
     public void checkIndex(int index){
-        //If index is not in boundaries -> throw exception
         if (index < 0 || index >= this.size){
             throw new IndexOutOfBoundsException();
         }
     }
     @Override
     public int size(){
-        //returning the size variable
         return this.size;
     }
+    @Override
+    public boolean contains(Object o){
+        for (int i = 0; i < this.size; i++){
+            if (array[i].equals(o)){
+                return true;
+            }
+        }   return false;
+    }
+    @Override
+    public void add(T item, int index) {
+        checkIndex(index);
+        if (this.size == array.length) {
+            Object[] newArray = new Object[array.length * 2];
+            System.arraycopy(array, 0, newArray, 0, index);
+            newArray[index] = item;
+            System.arraycopy(array, index, newArray, index + 1, size - index);
+            array = newArray;
+        } else {
+            System.arraycopy(array, index, array, index + 1, size - index);
+            array[index] = item;
+        }
+        this.size++;
+    }
+    @Override
+    public boolean remove(T item){
+        for (int i=0; i < this.size; i++){
+            if (array[i].equals(item)) {
+                System.arraycopy(array, i + 1, array, i, this.size - i - 1);
+                this.size--;
+                array[this.size] = null;
+                return true;
+            }
+        }
+        return false;
+    }
+    @Override
+    public void clear(){
+        this.array = new Object[5];
+        this.size = 0;
+    }
+    @Override
+    public int indexOf(Object o){
+        for (int i = 0; i < this.size; i++) {
+            if (array[i].equals(o)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    @Override
+    public int lastIndexOf(Object o){
+        for (int i = this.size - 1; i >= 0; i--) {
+            if (array[i].equals(o)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    @Override
+    public void sort(){
+        Arrays.sort((T[]) array, 0, this.size);
+    }
+
+    @Override
+    public void deleteDuplicates(){
+        for (int i = 0; i < this.size; i++){
+            for (int j = 1; j < this.size; j++){
+                if (array[i].equals(array[j])){
+                    System.arraycopy(array, j + 1, array, j, this.size - j - 1);
+                    this.size--;
+                }
+            }
+        }
+    }
+
 }
