7b9dba3fd9ed318f12c5385f14169abf47715b64
Mekentosh <m87022928044@gmail.com>
2023-07-24 03:25:43 +0600

    7

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,2 +1,16 @@
-public class MyLinkedList {
+public class MyLinkedList<E> implements MyList<E> {
+    private class Node{
+        E element;
+        Node next;
+        Node prev;
+        Node(E element, Node next, Node prev) {
+            this.element = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+    private Node head;
+    private Node tail;
+    private int size;
+
 }
