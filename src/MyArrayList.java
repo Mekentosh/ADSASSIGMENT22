1cc39d2389b9383156595d3a0ee68ac781420fca
Mekentosh <m87022928044@gmail.com>
2023-07-24 03:06:46 +0600

    4

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -3,8 +3,22 @@ public class MyArrayList<T> implements MyList<T> {
     private Object[] array;
     //Define an int variable called size to keep track of the number of elements in the list.
     private int size = 0;
+    public MyArrayList(){
+        this.array = new Object[5];
+        this.size = 0;
+    }
+    @Override
+    public void add(T item){
+        if (this.size == array.length){
+            Object[] newArray = new Object[array.length * 2];
 
+            System.arraycopy(array, 0, newArray, 0, this.size);
 
+            array = newArray;
+        }
+        array[this.size] = item;
+        this.size++;
+    }
 
 
 }
